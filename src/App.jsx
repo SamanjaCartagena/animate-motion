7846237095491f99578e60fd1c14c453e888bbf6
Samanja Cartagena <c.samanja09@Pabs.local>
2026-04-19 12:07:23 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { motion } from 'motion/react'
 import './App.css'
 import AnimatedButton from './AnimatedButton'
 import { FeaturesList } from './FeaturesList'
+import  DraggableCard from './DraggableCard'
+
+
 function App() {
  
   return (
@@ -10,6 +13,8 @@ function App() {
     <AnimatedButton>Click Me</AnimatedButton>
     <FeaturesList />
     <AnimatedButton damping={0}>Fuck me</AnimatedButton>
+    <br/>
+    <DraggableCard/>
     </>
   )
 }
--- src/DraggableCard.tsx
+++ src/DraggableCard.tsx
@@ -3,7 +3,8 @@ import {motion} from 'motion/react'
 export default function DraggableCard(){
     return (
         <div className='drag-container'>
-            <motion.div className='card'>Drag me!</motion.div>
+            <motion.div className='card' drag dragConstraints={{left:-80, right:80, top:40, bottom:40}}
+            dragElastic={0.5}>Drag me!</motion.div>
         </div>
     )
 }
